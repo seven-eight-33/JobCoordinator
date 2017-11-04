8f81f008fdaa9b050dc61a8c15fe6d4479f3a103
松谷 歩高 <>
2017-11-04 19:08:45 +0900

    test3

--- application/controllers/top.php
+++ application/controllers/top.php
@@ -18,14 +18,8 @@ class Top extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	 public function __construct()
-	 {
-		 parent::__construct();
-	 }
-
 	 public function index()
 	 {
-		 echo 'aaaaa';
-     //$this->load->view('top');
+     $this->load->view('top');
 	 }
 }
