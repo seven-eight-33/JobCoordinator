cb79818c1a3ec36472e8e551afb61924a22f7650
松谷 歩高 <>
2017-11-04 19:16:44 +0900

    一旦戻す

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Top extends CI_Controller {
+class Welcome extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,6 @@ class Top extends CI_Controller {
 	 */
 	 public function index()
 	 {
-     $this->load->view('top');
+     $this->load->view('welcome_message');
 	 }
 }
