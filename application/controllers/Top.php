ecd7ec87e09c451491e0aa9ced3780ea61f8f7e0
松谷 歩高 <>
2017-11-05 15:54:25 +0900

    view調整

--- application/controllers/Top.php
+++ application/controllers/Top.php
@@ -1,27 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-//require_once APPPATH. 'models/User.php';
 
 class Top extends CI_Controller {
-
-//	$modelUser = NULL;
 
 	public function __construct()
 	{
 		parent::__construct();
-		//$modelUser = new User();
 		$this->load->model('User', 'modelUser', TRUE);
 	}
 
 	public function index()
 	{
-//		$this->load->database();
-//		$query = $this->db->get('USER');
-		$userData = $this->modelUser->get_all_user();
-		foreach ($userData as $row)
-		{
-			echo $row->NAME1;
-		}
-		$this->load->view('top');
+		$data['result'] = $this->modelUser->get_all_user();
+		$this->load->view('top', $data);
 	}
 }
--- application/views/top.php
+++ application/views/top.php
@@ -73,7 +73,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div id="body">
 		<p>top調整成功！</p>
 	</div>
-
+	<?php foreach ($result as $results): ?>
+	  <p><?php echo $results->NAME1; ?></p>
+	<?php endforeach; ?>
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
