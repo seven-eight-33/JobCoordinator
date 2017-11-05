9c27805d2ed644cb1b67e92bbfa1add45f051360
松谷 歩高 <>
2017-11-05 14:15:11 +0900

    コメント削除

--- application/config/database.php
+++ application/config/database.php
@@ -95,27 +95,3 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-
-/*
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'mysql475.db.sakura.ne.jp',
-	'username' => 'h-matsuya',
-	'password' => '82ImZdq0f5QH5CLh',
-	'database' => 'h-matsuya_job_coordinator',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-*/
