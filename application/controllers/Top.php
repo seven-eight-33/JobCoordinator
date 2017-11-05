54fb628236c1df5343f1c48d8f1c6a66a4ed5e09
松谷 歩高 <>
2017-11-05 15:46:27 +0900

    model調整

--- application/controllers/Top.php
+++ application/controllers/Top.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require_once APPPATH. 'models/User.php';
+//require_once APPPATH. 'models/User.php';
 
 class Top extends CI_Controller {
 
--- application/models/User.php
+++ application/models/User.php
@@ -1,11 +1,10 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
 class User extends CI_Model
 {
     public function __construct()
     {
         parent::__construct();
+				$this->load->database();
     }
 
 		public function get_all_user()
