a13b16afb22d55033f5d7611e1bbe95f0a09dbec
松谷 歩高 <>
2017-11-11 14:31:20 +0900

    ヘッダ調整

--- application/views/header.php
+++ application/views/header.php
@@ -2,10 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
 	<title><?php echo $title;?></title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="format-detection" content="telephone=no">
+
+	<meta name="viewport" content="width=device-width,initial-scale=0.8">
+
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="http://h-matsuya.sakura.ne.jp/JobCoordinator/">
+	<meta property="og:image" content="">
+	<meta property="og:title" content="">
+	<meta property="og:description" content="">
+
 	<link href="css/common.css" rel="stylesheet" type="text/css">
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="js/common.js"></script>
