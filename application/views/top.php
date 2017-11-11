5b3369b92d2112ce505042b813db836b0e834e34
松谷 歩高 <>
2017-11-11 14:23:00 +0900

    コンテンツ部分調整

--- application/views/top.php
+++ application/views/top.php
@@ -1,16 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-	<link href="css/common.css" rel="stylesheet" type="text/css">
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/common.js"></script>
-</head>
-<body>
-
+?>
 <div id="container">
 	<h1>トップページ!</h1>
 
@@ -22,6 +12,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<?php endforeach; ?>
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
-
-</body>
-</html>
