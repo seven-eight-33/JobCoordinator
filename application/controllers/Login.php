f07bc91290eda06df5ba7e45983b039468f97da9
松谷 歩高 <>
2017-11-13 22:45:42 +0900

    lib修正

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,77 +3,67 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper('url', 'form');
-		$this->load->library('form_validation');
-		$this->load->model('User', 'modelUser', TRUE);
-		$this->load->library('login_lib');
-	}
-/*
-	public function login_check()
-	{
-		$userData = $this->modelUser->get_once_user($this->input->post("login_id"), $this->input->post("password"));
-		if(!empty($userData)){
-			return true;
-		}else{
-			$this->form_validation->set_message("login_check", "id または password を正しく入力してください。");
-			return false;
-		}
-	}
-*/
-	public function index()
-	{
-		if(empty($this->input->post('action'))){
-			$data['title'] = 'JobCoordinator-Login';
-			$data['result'] = $this->modelUser->get_all_user();
-			$this->load->view('header', $data);
-			$this->load->view('login', $data);
-			$this->load->view('footer', $data);
-		}else{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url', 'form');
+        $this->load->library('form_validation');
+        $this->load->model('User', 'modelUser', TRUE);
+//        $this->load->library('login_lib');
+    }
 
-			if($this->login_lib->login_validation($this->input->post())){
-				redirect('mypage');
-			}else{
-				$data['title'] = 'JobCoordinator-Login';
-				$this->load->view('header', $data);
-				$this->load->view('login', $data);
-				$this->load->view('footer', $data);
-			}
-/*
-			$config = [
-				[
-					'field' => 'login_id',
-					'label' => 'id',
-					'rules' => 'required|max_length[20]',
-					'errors' => [
-						'required' => 'id を入力してください。',
-						'max_length' => 'id または password を正しく入力してください。',
-					]
-				],
-				[
-					'field' => 'password',
-					'label' => 'password',
-					'rules' => 'required|max_length[20]|callback_login_check',
-					'errors' => [
-						'required' => 'password を入力してください。',
-						'max_length' => 'id または password を正しく入力してください。',
-						'callback_login_check' => 'id または password を正しく入力してください。',
-					]
-				]
-			];
-			$this->form_validation->set_rules($config);
+    public function login_check()
+    {
+        $userData = $this->modelUser->get_once_user($this->input->post("login_id"), $this->input->post("password"));
+        if(!empty($userData)){
+            return true;
+        }else{
+            $this->form_validation->set_message("login_check", "id または password を正しく入力してください。");
+            return false;
+        }
+    }
 
-			if($this->form_validation->run()){	//バリデーションエラーがなかった場合の処理
-				redirect('mypage');
-			}else{							//バリデーションエラーがあった場合の処理
-				$data['title'] = 'JobCoordinator-Login';
-				$this->load->view('header', $data);
-				$this->load->view('login', $data);
-				$this->load->view('footer', $data);
-			}
-*/
-		}
-	}
+    public function login_validation()
+    {
+        $config = [
+            [
+                'field' => 'login_id',
+                'label' => 'id',
+                'rules' => 'required|max_length[20]',
+                'errors' => [
+                    'required' => 'id を入力してください。',
+                    'max_length' => 'id または password を正しく入力してください。',
+                ]
+            ],
+            [
+                'field' => 'password',
+                'label' => 'password',
+                'rules' => 'required|max_length[20]|callback_login_check',
+                'errors' => [
+                    'required' => 'password を入力してください。',
+                    'max_length' => 'id または password を正しく入力してください。',
+                ]
+            ]
+        ];
+        $this->form_validation->set_rules($config);
+        return $this->form_validation->run();
+    }
+
+    public function index()
+    {
+        if(empty($this->input->post('action'))){
+            $data['title'] = 'JobCoordinator-Login';
+            $data['result'] = $this->modelUser->get_all_user();
+        }else{
+            if($this->login_lib->login_validation($this->input->post())){
+                redirect('mypage');
+            }else{
+                $data['title'] = 'JobCoordinator-Login';
+            }
+        }
+
+        $this->load->view('header', $data);
+        $this->load->view('login', $data);
+        $this->load->view('footer', $data);
+    }
 }
--- application/libraries/Login_lib.php
+++ application/libraries/Login_lib.php
@@ -4,54 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login_lib {
 
     protected $CI;
-    public $input;
 
     public function __construct()
     {
         $this->CI =& get_instance();
-
-        $this->CI->load->helper('url', 'form');
-        $this->CI->load->library('form_validation');
-        $this->CI->load->model('User', 'modelUser', TRUE);
-    }
-
-    public function login_check()
-    {
-        $userData = $this->CI->modelUser->get_once_user($this->input("login_id"), $this->input("password"));
-        if(!empty($userData)){
-            return true;
-        }else{
-            $this->CI->form_validation->set_message("login_check", "id または password を正しく入力してください。");
-            return false;
-        }
-    }
-
-    public function login_validation($params)
-    {
-        $this->input = $params;
-
-        $config = [
-            [
-                'field' => 'login_id',
-                'label' => 'id',
-                'rules' => 'required|max_length[20]',
-                'errors' => [
-                    'required' => 'id を入力してください。',
-                    'max_length' => 'id または password を正しく入力してください。',
-                ]
-            ],
-            [
-                'field' => 'password',
-                'label' => 'password',
-                'rules' => 'required|max_length[20]|callback_login_check',
-                'errors' => [
-                    'required' => 'password を入力してください。',
-                    'max_length' => 'id または password を正しく入力してください。',
-                ]
-            ]
-        ];
-        $this->CI->form_validation->set_rules($config);
-        return $this->CI->form_validation->run();
     }
 
 }
