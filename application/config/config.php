62942b0b0aaa63179f111ea53808c86149b3c383
松谷 歩高 <>
2017-11-18 18:00:17 +0900

    URL調整

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://h-matsuya.sakura.ne.jp/JobCoordinator/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
