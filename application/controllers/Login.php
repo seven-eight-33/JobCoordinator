7c13aef1201011030a846ebb3f2a458a3b66c45e
松谷 歩高 <>
2017-11-19 22:52:09 +0900

    function調整

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,7 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-    protected function __construct()
+    const LOGIN_START = 1;
+    const LOGIN_SUCCESS = 2;
+    const LOGIN_ERROR = 3;
+
+    public $viewType = 0;
+    public $viewData = NULL;
+
+    public function __construct()
     {
         parent::__construct();
         $this->load->helper('url', 'form');
@@ -12,7 +19,72 @@ class Login extends CI_Controller {
 //        $this->load->library('login_lib');
     }
 
-    protected function login_check()
+    public function _preprocess()
+    {
+        $res = 0;
+        if(empty($this->input->post('action'))){
+            $res = self::LOGIN_START;
+        }else{
+            if($this->login_validation()){
+                $res = self::LOGIN_SUCCESS;
+            }else{
+                $res = self::LOGIN_ERROR;
+            }
+        }
+        return $res;
+    }
+
+    public function _mainprocess()
+    {
+        switch($this->$viewType){
+            case self::LOGIN_START:
+                $this->viewData['title'] = 'JobCoordinator-Login';
+                $this->viewData['result'] = $this->modelUser->get_all_user();
+                break;
+            case self::LOGIN_SUCCESS:
+                // session 操作
+                redirect('mypage');
+                break;
+            case self::LOGIN_ERROR:
+                $this->viewData['title'] = 'JobCoordinator-Login';
+                break;
+            default:
+                break;
+        }
+    }
+
+    public function _main_view()
+    {
+        $this->load->view('header', $this->viewData);
+        $this->load->view('login', $this->viewData);
+        $this->load->view('footer', $this->viewData);
+    }
+
+    public function index()
+    {
+        $this->$viewType = $this->_preprocess();
+        $this->_mainprocess();
+        $this->_main_view();
+/*
+        if(empty($this->input->post('action'))){
+            $data['title'] = 'JobCoordinator-Login';
+            $data['result'] = $this->modelUser->get_all_user();
+        }else{
+            if($this->login_validation()){
+                redirect('mypage');
+            }else{
+                $data['title'] = 'JobCoordinator-Login';
+            }
+        }
+
+        $this->load->view('header', $data);
+        $this->load->view('login', $data);
+        $this->load->view('footer', $data);
+*/
+    }
+
+/********************* ↓ サブファンクション ↓ *********************/
+    public function login_check()
     {
         $userData = $this->modelUser->get_once_user($this->input->post("login_id"), $this->input->post("password"));
         if(!empty($userData)){
@@ -23,7 +95,7 @@ class Login extends CI_Controller {
         }
     }
 
-    protected function login_validation()
+    public function login_validation()
     {
         $config = [
             [
@@ -47,23 +119,5 @@ class Login extends CI_Controller {
         ];
         $this->form_validation->set_rules($config);
         return $this->form_validation->run();
-    }
-
-    protected function index()
-    {
-        if(empty($this->input->post('action'))){
-            $data['title'] = 'JobCoordinator-Login';
-            $data['result'] = $this->modelUser->get_all_user();
-        }else{
-            if($this->login_validation()){
-                redirect('mypage');
-            }else{
-                $data['title'] = 'JobCoordinator-Login';
-            }
-        }
-
-        $this->load->view('header', $data);
-        $this->load->view('login', $data);
-        $this->load->view('footer', $data);
     }
 }
