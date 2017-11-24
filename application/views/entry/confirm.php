ecf8d1e7eb929e9f4fde8dfc74eafdc07b2e1606
松谷 歩高 <>
2017-11-24 12:48:52 +0900

    selectbox修正

--- application/views/entry/confirm.php
+++ application/views/entry/confirm.php
@@ -8,23 +8,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <?php echo form_open('entry/confirm'); ?>
             <dl class="cf">
                 <dt>ユーザID</dt>
-                <dd><?php echo $user_id ?></dd>
+                <dd><?php echo $user_id; ?></dd>
                 <dt>氏名</dt>
-                <dd><?php echo $name1. " ". $name2 ?></dd>
+                <dd><?php echo $name1. " ". $name2; ?></dd>
                 <dt>氏名カナ</dt>
-                <dd><?php echo $name1_kana. " ". $name2_kana ?></dd>
+                <dd><?php echo $name1_kana. " ". $name2_kana; ?></dd>
                 <dt>性別</dt>
-                <dd><?php echo $sex_val ?></dd>
+                <dd><?php echo $sex_val; ?></dd>
                 <dt>郵便番号</dt>
-                <dd><?php echo $zip1. " - ". $zip2 ?></dd>
+                <dd><?php echo $zip1. " - ". $zip2; ?></dd>
                 <dt>住所</dt>
-                <dd><?php echo $pref_val. $address1. $address2 ?></dd>
+                <dd><?php echo $pref_val. $address1. $address2; ?></dd>
                 <dt>電話番号</dt>
-                <dd><?php echo $tel1. " - ". $tel2. " - ". $tel3 ?></dd>
+                <dd><?php echo $tel1. " - ". $tel2. " - ". $tel3; ?></dd>
                 <dt>メールアドレス</dt>
-                <dd><?php echo $mail ?></dd>
+                <dd><?php echo $mail; ?></dd>
                 <dt>パスワード</dt>
-                <dd><?php echo $password_val ?></dd>
+                <dd><?php echo $password_val; ?></dd>
             </dl>
             <div class="txtC">
                 <button type='submit' name='action' value='1'>confirm</button>
