601c59aacbc3c1cb1be34216644a842f311162bc
松谷 歩高 <>
2017-11-26 17:58:59 +0900

    自作ライブラリ適用

--- application/controllers/Entry/Input.php
+++ application/controllers/Entry/Input.php
@@ -48,8 +48,9 @@ class Input extends CI_Controller {
             case self::INPUT_START:     // 初期表示
                 if($this->session->has_userdata('fix_flg') && $this->session->userdata('fix_flg') == 1){
                     $this->input->post($this->session->userdata());
-                    var_dump($this->input->post());
-                    exit;
+                    $_POST = $this->session->userdata();
+//                    var_dump($this->input->post());
+//                    exit;
                 }
                 $this->viewData['title'] = 'JobCoordinator-Entry';
                 $this->viewData['pref_list'] = $this->config->item('pref_list');
