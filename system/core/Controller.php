761c372245dcb2f17cb7793a060ad180cc473c25
松谷 歩高 <>
2017-11-26 12:20:36 +0900

    自作ライブラリ適用

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -51,46 +51,62 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class CI_Controller {
 
-	/**
-	 * Reference to the CI singleton
-	 *
-	 * @var	object
-	 */
-	private static $instance;
+    /**
+     * Reference to the CI singleton
+     *
+     * @var	object
+     */
+    private static $instance;
 
-	/**
-	 * Class constructor
-	 *
-	 * @return	void
-	 */
-	public function __construct()
-	{
-		self::$instance =& $this;
+    /**
+     * Class constructor
+     *
+     * @return	void
+     */
+    public function __construct()
+    {
+        self::$instance =& $this;
 
-		// Assign all the class objects that were instantiated by the
-		// bootstrap file (CodeIgniter.php) to local class variables
-		// so that CI can run as one big super object.
-		foreach (is_loaded() as $var => $class)
-		{
-			$this->$var =& load_class($class);
-		}
+        // Assign all the class objects that were instantiated by the
+        // bootstrap file (CodeIgniter.php) to local class variables
+        // so that CI can run as one big super object.
+        foreach (is_loaded() as $var => $class)
+        {
+            $this->$var =& load_class($class);
+        }
 
-		$this->load =& load_class('Loader', 'core');
-		$this->load->initialize();
-		log_message('info', 'Controller Class Initialized');
-	}
+        $this->load =& load_class('Loader', 'core');
+        $this->load->initialize();
+        log_message('info', 'Controller Class Initialized');
+    }
 
-	// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 
-	/**
-	 * Get the CI singleton
-	 *
-	 * @static
-	 * @return	object
-	 */
-	public static function &get_instance()
-	{
-		return self::$instance;
-	}
+    /**
+     * Get the CI singleton
+     *
+     * @static
+     * @return	object
+     */
+    public static function &get_instance()
+    {
+        return self::$instance;
+    }
+
+    // 半角英数記号チェック
+    public function _alpha_numeric_symbol()
+    {
+        $target = $this->input->post("password");
+        if ($target == null) {
+            $this->form_validation->set_message('_alpha_numeric_symbol', '');
+            return false;
+        }
+        if(preg_match("/^[!-~]+$/", $target)){
+            return true;
+        }else{
+            $this->form_validation->set_message("_alpha_numeric_symbol", "パスワード は半角英数記号で入力してください。");
+            return false;
+        }
+    }
 
 }
