ad7aee1af93178772eabe284c784a1cc846b1907
松谷 歩高 <>
2017-12-09 15:33:44 +0900

    login処理修正

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -78,11 +78,11 @@ class Login extends CI_Controller {
         $userData = $this->modelUser->get_once_user($this->input->post("login_id"));
         if(!empty($userData)){
 
-var_dump($userData);
-exit;
+//var_dump($userData);
+//exit;
 
-            $pass_hash = $this->form->_my_hash($this->input->post("password"), $userData['SALT'], $userData['STRETCH']);
-            if($userData['PASSWORD'] == $pass_hash) $res = true;
+            $pass_hash = $this->form->_my_hash($this->input->post("password"), $userData[0]['SALT'], $userData[0]['STRETCH']);
+            if($userData[0]['PASSWORD'] == $pass_hash) $res = true;
         }
 
         if(!$res){
