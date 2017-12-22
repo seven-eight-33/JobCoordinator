83595e429a18a801976f43c81cba5de6b2cdbb0b
松谷 歩高 <>
2017-12-22 21:03:31 +0900

    ログインチェック周り修正

--- application/controllers/Member/Mypage.php
+++ application/controllers/Member/Mypage.php
@@ -12,7 +12,6 @@ class Mypage extends CI_Controller {
     {
         parent::__construct();
         $this->load->model('User', 'modelUser', TRUE);
-        $this->load->library('controllers/Login/login_lib');
     }
 
 /********************* ↓ routes function ↓ *********************/
@@ -26,6 +25,9 @@ class Mypage extends CI_Controller {
 /********************* ↓ main function ↓ *********************/
     protected function _preprocess()
     {
+        // ログインチェック
+        $this->login_lib->_is_logged_in($_SERVER["HTTP_HOST"]. $_SERVER["REQUEST_URI"]);
+
         $res = 0;
         if(empty($this->input->post('action'))){
             $res = self::MYPAGE_START;
