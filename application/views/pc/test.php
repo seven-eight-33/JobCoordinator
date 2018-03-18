05a1599557f465fdd11534f1f33e5e70c1a16e0f
松谷 歩高 <>
2018-03-18 14:48:25 +0900

    test微調整

--- application/views/pc/test.php
+++ application/views/pc/test.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div id="body">
         <p>test_contents！</p>
         <?php foreach ($results as $schedule): ?>
-            <p><?php echo $schedule->start; ?>：<?php echo $schedule->summary; ?></p>
+            <p><?php echo $schedule['start']; ?>：<?php echo $schedule['summary']; ?></p>
         <?php endforeach; ?>
     </div>
 </div>
